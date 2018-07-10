061549b45f5885bd28dddaef714cab04061d4044
elenaojea <elenaojea@gmail.com>
2018-07-10 15:44:10 +0200

    code on figure 2 AC

--- figures_vul.R
+++ figures_vul.R
@@ -7,6 +7,10 @@
 library(tidyverse)
 library(ggplot2)
 library(ggrepel)
+library(tidyr)
+library(tibble)
+library(scales)
+library(dplyr)
 
 ##Read final Vulnerability index and ecological vulnerability
 
@@ -54,5 +58,48 @@ f1
 ggsave("Figures/Figure 1.png", dpi = 600)
 
 
+##Figure 2 Adaptive Capacity indicators
 
+#open csv indicators
+
+data_SAC <- read.csv("data_vul/SAC indicators.csv", sep=";")                 #open .csv SAC indicators
+
+##add the SAC score
+data_SAC$guild <- as.character(data_SAC$guild)                            #to convert column name "guild" into  a character
+data_SAC$guild[data_SAC$guild == "Moana "] <-"Moaña"                      #to correct Moaña    to be common among data files 
+data_SAC <- data_SAC[1:6,]                                                #to delete average and min and max values
+
+data$guild <- as.character(data$guild)                                     #to convert guild from factor to character in data to merge
+data_SAC <- left_join(data_SAC, data, by="guild")                            #to combine data_SAC and data by the joint column "guild"
+
+#need to convert wide data into long format for ggplot:
+data_long <- gather(data_SAC, indicator, value, experience:monoculture, factor_key=TRUE)
+data_long
+
+
+data_long <- data_long %>%
+                  group_by(indicator) %>%
+                    mutate(suma = sum(value))
+data_long$suma <- as.factor(data_long$suma)
+data_long$suma <- as.numeric(data_long$suma)
+
+g <- ggplot(x, aes(reorder(variable, value), value))
+
+f2 <-  ggplot(data_long, aes(reorder(indicator, suma), value, fill = guild)) + 
+  geom_bar(stat = "identity", position = "stack", width=0.7)+
+  #geom_text(aes(label = value), position = position_dodge(width = .8), size=3, vjust = -0.5)+
+  xlab(NULL)+
+  ylab(NULL)+
+  #ylim(0,30)+
+  scale_fill_brewer(palette="Spectral")+
+  theme_bw()+
+  coord_flip()+
+  facet_wrap(~guild, scales="free_y") +
+  scale_fill_discrete(guide=FALSE)
+f2
+
+
+##Saving the figure in /Figures:
+
+ggsave("Figures/Figure 2.png", dpi = 600)
 
